7196aef819086d9756df676a30113679f1dcf920
Bacon <l.trovato1@studenti.unisa.it>
2025-12-07 19:25:53 +0100

    implementazione doxygen InterfaceGestoreIO

---
+++ src/main/java/it/unisa/diem/ingegneriadelsoftware/repository/InterfaceGestoreIO.java
@@ -0,0 +1,28 @@
+package com.tuoprogetto.biblioteca.repository;
+import java.util.List;
+
+/**
+ * @interface InterfaceGestoreIO
+ * @brief Interfaccia per la gestione dell'Input/Output dei dati.
+ * @tparam T Il tipo di dato da gestire.
+ */
+public interface InterfaceGestoreIO<T> {
+
+    /**
+     * @brief Salva una lista di oggetti sul file.
+     * @param [in] nomeFile Il percorso o nome del file di destinazione.
+     * @param [in] dati La lista di oggetti generici T da salvare.
+     * @pre La lista dati non deve essere null.
+     * @post I dati sono stati salvati correttamente.
+     */
+    void salvaDati(String nomeFile, List<T> dati);
+
+    /**
+     * @brief Carica una lista di oggetti dal file.
+     * @param [in] nomeFile Il percorso o nome del file da leggere.
+     * @return Una lista contenente gli oggetti caricati, altrimenti restituisce una lista vuota.
+     * @pre Il parametro 'nomeFile' deve essere una stringa valida.
+     * @post Viene restituita un'istanza di List.
+     */
+    List<T> caricaDati(String nomeFile);
+}
