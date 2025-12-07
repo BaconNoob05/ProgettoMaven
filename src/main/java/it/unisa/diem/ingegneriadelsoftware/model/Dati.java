5469a14587692791aa832e55a3a690e7ea480d42
15M-I <matteoiandiorio15@gmail.com>
2025-12-07 18:29:02 +0100

    inizio implementazione di dati.java con commenti doxygen

--- src/main/java/it/unisa/diem/ingegneriadelsoftware/model/Dati.java
+++ src/main/java/it/unisa/diem/ingegneriadelsoftware/model/Dati.java
@@ -1,1 +1,15 @@
+package it.unisa.diem.ingegneriadelsoftware;
+import java.io.Serializable;
 
+/**
+ * @file Dati.java
+ * @brief Scheletro base per i modelli di dominio.
+ */
+public abstract class Dati implements InterfaceID, Serializable {
+    /**
+     * @brief Identificativo univoco per la serializzazione.
+     * @details Garantisce la compatibilità tra l'oggetto serializzato e la classe 
+     * caricata durante la fase di deserializzazione. Se questo ID non corrisponde 
+     * a quello dell'oggetto salvato, viene lanciata una InvalidClassException.
+     */
+    private static final long serialVersionUID = 1L;
