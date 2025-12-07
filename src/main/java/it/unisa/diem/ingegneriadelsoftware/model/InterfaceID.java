50e5b69c66ef89f959d6482d8f04adedd48cbf8a
15M-I <matteoiandiorio15@gmail.com>
2025-12-07 19:03:12 +0100

    efinita interfaccia InterfaceID per identificazione entità
    Creata l'interfaccia InterfaceID :
    Espone il metodo getId() per garantire l'univocità delle entità del sistema.
    Inclusa documentazione in stile Doxygen.

---
+++ src/main/java/it/unisa/diem/ingegneriadelsoftware/model/InterfaceID.java
@@ -0,0 +1,14 @@
+package it.unisa.diem.ingegneriadelsoftware;
+
+/**
+ * @class InterfaceID
+ * @brief Interfaccia funzionale per l'identificazione univoca delle diverse entità del sistema.
+ */
+public interface InterfaceID {
+
+    /**
+     * @brief Restituisce l'identificativo univoco dell'entità.
+     * @return Una stringa che rappresenta l'identificativo univoco dell'oggetto altrimenti restituisce un valore nullo.
+     */
+    String getId();
+}
