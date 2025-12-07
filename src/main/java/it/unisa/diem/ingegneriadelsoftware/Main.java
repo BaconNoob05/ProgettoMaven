48b431256fcaddab3b81a2acc557ceefbf515938
15M-I <matteoiandiorio15@gmail.com>
2025-12-07 18:00:34 +0100

    implementazione main con commenti doxygen

--- src/main/java/it/unisa/diem/ingegneriadelsoftware/Main.java
+++ src/main/java/it/unisa/diem/ingegneriadelsoftware/Main.java
@@ -1,1 +1,32 @@
+package com.tuoprogetto.biblioteca;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @class Main
+ * @brief Classe principale per l'avvio dell'applicazione Biblioteca.
+ * @details Si occupa di inizializzare l'architettura MVC,
+ * configurando i repository, i service e le viste, per poi avviare l'interfaccia grafica JavaFX.
+ */
+public class Main extends Application {
+
+    /**
+     * @brief Metodo di avvio dell'applicazione JavaFX.
+     * @details Costruisce l'intera gerarchia delle dipendenze:
+     * Repository, Service, View e Controller.
+     * Infine, assembla la scena principale e la mostra a video.
+     * @param [in] primaryStage Lo stage principale fornito da JavaFX.
+     * @pre Il sistema deve avere i permessi di lettura/scrittura sui file dati.
+     * @post L'applicazione grafica viene mostrata all'utente.
+     */
+    @Override
+    public void start(Stage primaryStage) { }
+
+    /**
+     * @brief Metodo principale di ingresso .
+     * @param [in] args Argomenti da riga di comando.
+     * @post L'applicazione viene lanciata.
+     */
+    public static void main(String[] args) { }
+}
