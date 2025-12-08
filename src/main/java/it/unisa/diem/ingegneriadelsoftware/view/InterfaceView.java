5f9b8ee05d9e608c4aef7ba29b18871c68342264
AlePica64 <alessandropicariello53@gmail.com>
2025-12-08 21:39:39 +0100

    Implementazione Doxygen InterfaceView

---
+++ src/main/java/it/unisa/diem/ingegneriadelsoftware/view/InterfaceView.java
@@ -0,0 +1,13 @@
+package it.unisa.diem.ingegneriadelsoftware.view;
+
+import java.util.List;
+
+/**
+ * @interface InterfaceView
+ * @brief Interfaccia generica che definisce il contratto per le viste.
+ * @tparam T Il tipo di dato visualizzato nella vista.
+ */
+public interface InterfaceView<T> {
+
+   
+}
