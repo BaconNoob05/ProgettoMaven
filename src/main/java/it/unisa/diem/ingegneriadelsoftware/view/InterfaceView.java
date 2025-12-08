2a1620fec4890cfc78eeaf4e81815bbb4d5f960b
piano <piano@LAPTOP-VVHBPSC6.mshome.net>
2025-12-08 21:42:52 +0100

    Implementazione Doxygen metodi InterfaceView

--- src/main/java/it/unisa/diem/ingegneriadelsoftware/view/InterfaceView.java
+++ src/main/java/it/unisa/diem/ingegneriadelsoftware/view/InterfaceView.java
@@ -9,5 +9,35 @@ import java.util.List;
  */
 public interface InterfaceView<T> {
 
-   
+    /**
+     * @brief Visualizza una lista di elementi nella vista.
+     * @param [in] lista La lista di oggetti T da mostrare.
+     * @pre La lista non può essere null.
+     * @post La vista riflette il contenuto della lista passata.
+     */
+    void mostraLista(List<T> lista);
+
+    /**
+     * @brief Recupera l'elemento attualmente selezionato dall'utente.
+     * @return L'oggetto selezionato, altrimenti restituisce un valore nullo.
+     * @pre La vista deve essere inizializzata.
+     * @post Lo stato della selezione non cambia.
+     */
+    T getElementoSelezionato();
+
+    /**
+     * @brief Ottiene il testo inserito nel campo di ricerca.
+     * @return Il testo contenuto nel campo di ricerca, altrimenti restituisce un valore nullo.
+     * @pre Il componente di ricerca deve esistere.
+     * @post Nessuna modifica alla UI.
+     */
+    String getCampoCerca();
+
+    /**
+     * @brief Mostra un messaggio all'utente.
+     * @param [in] messaggio Il testo da visualizzare.
+     * @pre Il messaggio non deve essere null.
+     * @post L'utente visualizza il responso.
+     */
+    void mostraMessaggio(String messaggio);
 }
