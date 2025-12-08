2ce11eec90ac08e9892a7f73e4496849d8abd23e
AlePica64 <alessandropicariello53@gmail.com>
2025-12-08 21:47:18 +0100

    Modifica implementazione Doxygen metodi InterfaceView

--- src/main/java/it/unisa/diem/ingegneriadelsoftware/view/InterfaceView.java
+++ src/main/java/it/unisa/diem/ingegneriadelsoftware/view/InterfaceView.java
@@ -4,23 +4,23 @@ import java.util.List;
 
 /**
  * @interface InterfaceView
- * @brief Interfaccia generica che definisce il contratto per le viste.
- * @tparam T Il tipo di dato visualizzato nella vista.
+ * @brief Interfaccia generica che definisce il contratto per le view.
+ * @tparam T Il tipo di dato visualizzato nella view.
  */
 public interface InterfaceView<T> {
 
     /**
-     * @brief Visualizza una lista di elementi nella vista.
+     * @brief Visualizza una lista di elementi nella view.
      * @param [in] lista La lista di oggetti T da mostrare.
      * @pre La lista non può essere null.
-     * @post La vista riflette il contenuto della lista passata.
+     * @post La view riflette il contenuto della lista passata.
      */
     void mostraLista(List<T> lista);
 
     /**
      * @brief Recupera l'elemento attualmente selezionato dall'utente.
      * @return L'oggetto selezionato, altrimenti restituisce un valore nullo.
-     * @pre La vista deve essere inizializzata.
+     * @pre La view deve essere inizializzata.
      * @post Lo stato della selezione non cambia.
      */
     T getElementoSelezionato();
@@ -28,7 +28,7 @@ public interface InterfaceView<T> {
     /**
      * @brief Ottiene il testo inserito nel campo di ricerca.
      * @return Il testo contenuto nel campo di ricerca, altrimenti restituisce un valore nullo.
-     * @pre Il componente di ricerca deve esistere.
+     * @pre L'elemento da cercare deve esistere.
      * @post Nessuna modifica alla UI.
      */
     String getCampoCerca();
