d10f6df52b26c5d3c7f763ea534680d30f4be77d
AlePica64 <alessandropicariello53@gmail.com>
2025-12-10 21:47:49 +0100

    Modifica LibroControllerTest

--- src/test/java/it/unisa/diem/ingegneriadelsoftware/controller/LibroControllerTest.java
+++ src/test/java/it/unisa/diem/ingegneriadelsoftware/controller/LibroControllerTest.java
@@ -1,24 +1,14 @@
 package it.unisa.diem.ingegneriadelsoftware.controller;
 
 
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import java.util.Collections;
-import java.util.List;
 
+import org.junit.jupiter.api.*;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LibroControllerTest {
 
-    @Mock
-    private LibroView mockView; 
-
-    @Mock
-    private LibroService mockService; 
-
-    private LibroController controller; 
+  private LibroController controller;
+  private LibroService service;
 
 }
