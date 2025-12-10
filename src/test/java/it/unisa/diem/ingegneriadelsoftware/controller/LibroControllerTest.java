808dfb7aa62fc24e3a739ef1f1917cdf31242f9b
piano <piano@LAPTOP-VVHBPSC6.mshome.net>
2025-12-10 21:16:24 +0100

    Implementazione LibroControllerTest

---
+++ src/test/java/it/unisa/diem/ingegneriadelsoftware/controller/LibroControllerTest.java
@@ -0,0 +1,15 @@
+package it.unisa.diem.ingegneriadelsoftware.controller;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import java.util.Collections;
+import java.util.List;
+
+
+class LibroControllerTest {
+
+}
