9a3b170616102324c3c015009ca22fd5f21260fb
AlePica64 <alessandropicariello53@gmail.com>
2025-12-10 21:09:06 +0100

    Implementazione BaseServiceTest

---
+++ src/test/java/it/unisa/diem/ingegneriadelsoftware/service/BaseServiceTest.java
@@ -0,0 +1,12 @@
+package it.unisa.diem.ingegneriadelsoftware.service;
+
+import it.unisa.diem.ingegneriadelsoftware.repository.InterfaceRepository;
+import it.unisa.diem.ingegneriadelsoftware.test.ElementoTest;
+import org.junit.jupiter.api.*;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class BaseServiceTest {
+
+}
