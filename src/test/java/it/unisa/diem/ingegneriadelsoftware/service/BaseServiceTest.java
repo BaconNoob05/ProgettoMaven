00552a4142c52d54b90c5e7b845c5ca178594aff
piano <piano@LAPTOP-VVHBPSC6.mshome.net>
2025-12-11 21:21:09 +0100

    Modifica implementazione metodi di test per il metodo cerca della classe TestBaseService

--- src/test/java/it/unisa/diem/ingegneriadelsoftware/service/BaseServiceTest.java
+++ src/test/java/it/unisa/diem/ingegneriadelsoftware/service/BaseServiceTest.java
@@ -2,88 +2,146 @@ package it.unisa.diem.ingegneriadelsoftware.service;
 
 import it.unisa.diem.ingegneriadelsoftware.repository.InterfaceRepository;
 import org.junit.jupiter.api.*;
-import java.util.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-
-package it.unisa.diem.ingegneriadelsoftware.service;
-
-import it.unisa.diem.ingegneriadelsoftware.repository.InterfaceRepository;
-import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.*;
 
 class TestBaseService {
 
-    private BaseService<Utente> service;
-    private RepositoryStub<Utente> repo;
+    private BaseService<Utente> userService;
+    private RepositoryStub<Utente> userRepo;
     Utente u1,u2;
+
+    private BaseService<Libro> bookService;
+    private RepositoryStub<Libro> bookRepo;
+    private Libro l1, l2;
 
     @BeforeEach
     void setup() {
-        repo = new RepositoryStub<>();
-        service = new BaseService<>(repo);
+        userRepo = new RepositoryStub<>();
+        userService = new BaseService<>(userRepo);
         u1 = new Utente("Luigi","Bianchi","0612709564","gigi@uni.it");
         u2 = new Utente("Marco","Verdi","0612709783","verdi@uni.it");
-        service.salva(u1);
-        service.salva(u2);
+        userService.salva(u1);
+        userService.salva(u2);
+
+        bookRepo = new RepositoryStub<>();
+        bookService = new BaseService<>(bookRepo);
+
+        List<String> autoriLibro1 = Arrays.asList("Michel Foucault");
+        List<String> autoriLibro2 = Arrays.asList("Hannah Arendt");
+
+        l1 = new Libro("Il potere e la parola", autoriLibro1,1978,"978-0356429785",2);
+        l2 = new Libro("La banalità del male", autoriLibro2, 1963, "978-0299865341",10);
+        bookService.salva(l1);
+        bookService.salva(l2);
     }
 
 
+//TEST PER IL METODO cerca(String id) :
+
+//Test relativi alla ricerca di utenti
+
     @Test
-    void testCercaIdEsistente() {
-       Utente utenteTrovato = service.cerca("0612709564");
+    void testCercaIdEsistenteUtente() {
+       Utente utenteTrovato = userService.cerca("0612709564");
         assertNotNull(utenteTrovato);
         assertEquals("0612709564", utenteTrovato.getId());
     }
 
 @Test
-    void testCercaIdNonEsistente() {
-       Utente utenteTrovato = service.cerca("0000000000");
+    void testCercaIdNonEsistenteUtente() {
+       Utente utenteTrovato = userService.cerca("0000000000");
         assertNull(utenteTrovato);
     }
 
 @Test
-    void testCercaIdNull() {
-        Utente utenteTrovato = service.cerca(null);
+    void testCercaIdNullUtente() {
+        Utente utenteTrovato = userService.cerca(null);
         assertNull(utenteTrovato);
     }
 
 @Test
-    void testCercaIdVuoto() {
-        Utente utenteTrovato = service.cerca("");
+    void testCercaIdVuotoUtente() {
+        Utente utenteTrovato = userService.cerca("");
         assertNull(utenteTrovato);
     }
 
 @Test
-    void testCercaRepositoryVuoto() {
+    void testCercaRepositoryVuotoUtente() {
 
         RepositoryStub<Utente> emptyRepo = new RepositoryStub<>();
         BaseService<Utente> emptyService = new BaseService<>(emptyRepo);
         
         Utente utenteTrovato = emptyService.cerca("*");
-
         assertNull(utenteTrovato);
     }
 
 @Test
-    void testCercaPiùElementiPresenti() {
+    void testCercaPiùUtentiPresenti() {
         
-        assertEquals(u1, service.cerca("0612709564"));
-        assertEquals(u2, service.cerca("0612709783"));
+        assertEquals(u1, userService.cerca("0612709564"));
+        assertEquals(u2, userService.cerca("0612709783"));
     }
 
 @Test
-    void testCercaElementoAggiornato() {
+    void testCercaUtenteAggiornato() {
         Utente u = new Utente("Mario", "Rossi", "0612709783", "rossi@uni.it");
-        service.salva(u);
+        userService.salva(u);
 
         Utente uAggiornato = new Utente("Mario", "Rossi", "0612709783", "mariorossi@uni.it");
-        service.modifica(uAggiornato); // usa inserisciOAggiorna nel repo
+        userService.modifica(uAggiornato); // usa inserisciOAggiorna nel repo
 
-        Utente utenteTrovato = service.cerca("0612709783");
+        Utente utenteTrovato = userService.cerca("0612709783");
 
         assertEquals("mariorossi@uni.it", utenteTrovato.getEmail());
     }
 
+//Test relativi alla ricerca di libri
+@Test
+    void testCercaIdEsistenteLibro() {
+        Libro libroTrovato = libroService.cerca("978-0356429785");
+        assertNotNull(libroTrovato);
+        assertEquals("978-0356429785", libroTrovato.getId());
+        assertEquals("Il potere e la parola", libroTrovato.getTitolo());
+    }
+
+@Test
+    void testCercaIdNonEsistenteLibro() {
+        Libro libroTrovato = libroService.cerca("000-0000000000");
+        assertNull(libroTrovato);
+    }
+
+@Test
+    void testCercaIdVuotoLibro() {
+        Libro libroTrovato = libroService.cerca("");
+        assertNull(libroTrovato);
+    }
+
+@Test
+    void testCercaRepositoryVuotoLibro() {
+        RepositoryStub<Libro> emptyRepo = new RepositoryStub<>();
+        BaseService<Libro> emptyService = new BaseService<>(emptyRepo);
+
+        Libro libroTrovato = emptyService.cerca("*");
+        assertNull(libroTrovato);
+    }
+
+@Test
+    void testCercaPiùLibriPresenti() {
+        assertEquals(l1, bookService.cerca("978-0356429785"));
+        assertEquals(l2, bookService.cerca("978-0299865341"));
+    }
+
+@Test
+    void testCercaLibroAggiornato() {
+        List<String> autoriLibro = Arrays.asList("Vittorio Alfieri");
+        Libro l = new Libro("Vita scritta da Esso", autoriLibro, 1804, "978-0366218820", 3);
+        bookService.salva(l);
+        
+        Libro lAggiornato = new Libro("Vita scritta da Esso", autoriLibro, 1806, "978-0366218820", 6);
+        bookService.salva(lAggiornato);
+        Libro lTrovato = libroService.cerca("978-0366218820");
+        assertEquals(6, lTrovato.getCopie());
+    }
 }
+
