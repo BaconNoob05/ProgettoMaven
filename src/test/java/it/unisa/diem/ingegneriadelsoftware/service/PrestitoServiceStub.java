80adaaad3017c59746deb18a16918fb237b2d864
AlePica64 <alessandropicariello53@gmail.com>
2025-12-12 13:03:46 +0100

    Implementazione PrestitoServiceStub

---
+++ src/test/java/it/unisa/diem/ingegneriadelsoftware/service/PrestitoServiceStub.java
@@ -0,0 +1,53 @@
+package it.unisa.diem.ingegneriadelsoftware.service;
+
+import it.unisa.diem.ingegneriadelsoftware.model.Prestito;
+import it.unisa.diem.ingegneriadelsoftware.model.Utente; 
+import it.unisa.diem.ingegneriadelsoftware.model.Libro;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
+public class PrestitoServiceStub extends PrestitoService {
+        public List<Prestito> dB = new ArrayList<>();
+        public boolean registraPrestitoChiamato = false;
+        public boolean registraRestituzioneChiamato = false;
+        
+        public PrestitoServiceStub() { 
+            super(null, null); 
+        }
+
+        @Override
+        public void registraPrestito(Utente utente, Libro libro, LocalDate dataPrevista) {
+            this.registraPrestitoChiamato = true;
+            Prestito nuovo = new Prestito(utente, libro, dataPrevista);
+            dB.add(nuovo);
+        }
+
+        @Override
+        public void registraRestituzione(Prestito prestito, LocalDate dataEffettiva) {
+            this.registraRestituzioneChiamato = true;
+
+            for (Prestito p : dB) {
+                //Impiego del metodo getId() della classe Prestito
+                if (p.getId() != null && p.getId().equals(prestito.getId())) {
+                    p.registraRestituzione(dataEffettiva);
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public List<Prestito> listaPrestitiAttivi() {
+            List<Prestito> attivi = new ArrayList<>();
+            for (Prestito p : dB) {
+                if (p.getDataEffettiva() == null) 
+                    attivi.add(p);
+            }
+            return attivi;
+        }
+        
+        @Override
+        public List<Prestito> getAll() {
+            return new ArrayList<>(dB);
+        }
+    }
