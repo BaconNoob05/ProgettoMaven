e2d98a00f521b4dbd3a4ef4906495bd6feeabbe9
15M-I <matteoiandiorio15@gmail.com>
2025-12-13 13:39:09 +0100

    aggiunti commenti doxygen informativi per datiStub

--- src/test/java/it/unisa/diem/ingegneriadelsoftware/model/DatiStub.java
+++ src/test/java/it/unisa/diem/ingegneriadelsoftware/model/DatiStub.java
@@ -1,33 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package it.unisa.diem.ingegneriadelsoftware.model;
 
 import java.io.Serializable;
 
 /**
- *
- * @author danie
+ * @class DatiStub
+ * @brief Creare un oggetto finto che simula il comportamento di Dati.
  */
 public class DatiStub implements InterfaceID, Serializable{
+    /**
+     * @brief Identificativo dell'oggetto Stub.
+     */
     private String id;
-    
+    /**
+     * @brief Costruttore della classe DatiStub.
+     */
     public DatiStub(String id){
         this.id=id;
     }
-    
+   /**
+     * @brief Restituisce l'identificativo dell'oggetto Stub.
+     * @see InterfaceID#getId()
+     */
     @Override
     public String getId() {
         return id;
     }
-    
+    /**
+     * @brief Restituisce una rappresentazione con una stringa dell'oggetto Stub.
+     */
     @Override
     public String toString() {
         return "Test{" + "id='" + id + '\'' + '}';
     }
-    
+    /**
+     * @brief Confronta l'oggetto Stub con un altro oggetto.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
