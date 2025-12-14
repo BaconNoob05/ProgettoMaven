75a1d99d76e39e2fdd8cdb4542b07e31366b288d
15M-I <matteoiandiorio15@gmail.com>
2025-12-14 10:13:20 +0100

    commenti per gestorefilestub

--- src/test/java/it/unisa/diem/ingegneriadelsoftware/repository/GestoreFileStub.java
+++ src/test/java/it/unisa/diem/ingegneriadelsoftware/repository/GestoreFileStub.java
@@ -1,16 +1,28 @@
 package it.unisa.diem.ingegneriadelsoftware.repository; 
 import java.util.*;
 
-// Stub interfaccia gestoreIO
+/**
+ * @brief Implementazione Stub di InterfaceGestoreIO.
+ * @class GestoreFileStub
+ */
 class GestoreFileStub<T> implements InterfaceGestoreIO<T> {
 
+    /**
+     * @brief Una lista utilizzata per simulare il salvataggio dei dati.
+     */
     private List<T> memoria = new ArrayList<>();
 
+    /**
+     * @brief Simula il salvataggio dei dati.
+     */
     @Override
     public void salvaDati(String nomeFile, List<T> dati) {
         memoria = new ArrayList<>(dati);
     }
 
+    /**
+     * @brief Simula il caricamento dei dati.
+     */
     @Override
     public List<T> caricaDati(String nomeFile) {
         return new ArrayList<>(memoria);
