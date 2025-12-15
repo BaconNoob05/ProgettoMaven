bb2b16c0a016b1c689f56fde2bb7a15d9498fec6
AlePica64 <alessandropicariello53@gmail.com>
2025-12-15 04:01:24 +0100

    Correzione documentazione Doxygen Dati

--- src/main/java/it/unisa/diem/ingegneriadelsoftware/model/Dati.java
+++ src/main/java/it/unisa/diem/ingegneriadelsoftware/model/Dati.java
@@ -2,16 +2,20 @@ package it.unisa.diem.ingegneriadelsoftware.model;
 import java.io.Serializable;
 
 /**
- * @file Dati.java
+ * @class Dati
  * @brief Scheletro base per i modelli di dominio.
+ * @details Classe astratta che implementa l'interfaccia InterfaceID per l'identificazione 
+ * e Serializable per permettere il salvataggio dei dati.
+ * @see InterfaceID
  */
 public abstract class Dati implements InterfaceID, Serializable {
   
     /**
      * @brief Restituisce l'identificativo univoco dell'oggetto.
-     * Metodo astratto che deve essere utilizzato da tutte le sottoclassi
+     * @details Metodo astratto che deve essere implementato da tutte le sottoclassi
      * per garantire che ogni entità abbia una chiave univoca.
-     * @return L'ID univoco dell'oggetto altrimenti un valore nullo.
+     * @return L'ID univoco dell'oggetto, altrimenti restituisce il valore null.
+     * @see InterfaceID#getId()
      */
     @Override
     public abstract String getId();
@@ -20,7 +24,7 @@ public abstract class Dati implements InterfaceID, Serializable {
      * @brief Restituisce una rappresentazione in formato stringa dell'oggetto.
      * @details Questo metodo è destinato ad essere sovrascritto dalle sottoclassi per fornire dettagli specifici
      * sui dati contenuti nell'oggetto.
-     * @return Una stringa che rappresenta lo stato dell'oggetto altrimenti restituisce un valore nullo.
+     * @return Una stringa che rappresenta lo stato dell'oggetto, altrimenti restituisce il valore null.
      * @pre L'oggetto deve essere stato istanziato correttamente.
      * @post Lo stato interno dell'oggetto rimane invariato.
      */
